6dcf5c72e7a75e3c4f3e90afcabc15c9e808cb0a
HyperPandax <lisannereilman@gmail.com>
2020-03-05 12:40:20 +0100

    randomize map + load from resources

--- Assets/Scripts/GameGenerator.cs
+++ Assets/Scripts/GameGenerator.cs
@@ -3,26 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameGenerator : MonoBehaviour{
-    //[SerializeField] private Transform levelPart_1;
-    //[SerializeField] private Transform levelPart_2;
-    //[SerializeField] private Transform levelPart_3;
-
-
+   
     [SerializeField] private Transform Player;
-
-    [SerializeField] private Transform levelG1;
-    [SerializeField] private Transform levelG2;
-    [SerializeField] private Transform levelG3;
-    [SerializeField] private Transform levelG4;
-    [SerializeField] private Transform levelG5;
-    [SerializeField] private Transform levelG6;
-
-    public GameObject[] allInstanciatedParts;
 
     List<Transform> levelParts;
     List<Vector3> levelPartsPositions;
-
-    //public ArrayList LevelParts = new ArrayList();
 
     [SerializeField] private int partGapx = 128;
     [SerializeField] private int partGapy = 0;
@@ -30,18 +15,23 @@ public class GameGenerator : MonoBehaviour{
 
     [SerializeField] private Vector3 lastPos;
 
-    private void Start()
-    {
+    private void Start(){
         levelParts = new List<Transform>();
-        levelParts.Add(levelG1);
-        levelParts.Add(levelG2);
-        levelParts.Add(levelG3);
-        levelParts.Add(levelG4);
-        levelParts.Add(levelG5);
-        levelParts.Add(levelG6);
-        print("*levelparts: " + levelParts +  " *Levelparts.Count: " + levelParts.Count);
-        
 
+        //Load all levelpart (Assets/Resources/Prefabs/levelParts)
+        var part1 = Resources.Load<Transform>("Prefabs/levelParts/G1");
+        levelParts.Add(part1);
+        var part2 = Resources.Load<Transform>("Prefabs/levelParts/G2");
+        levelParts.Add(part2);
+        var part3 = Resources.Load<Transform>("Prefabs/levelParts/G3");
+        levelParts.Add(part3);
+        var part4 = Resources.Load<Transform>("Prefabs/levelParts/G4");
+        levelParts.Add(part4);
+        var part5 = Resources.Load<Transform>("Prefabs/levelParts/G5");
+        levelParts.Add(part5);
+        var part6 = Resources.Load<Transform>("Prefabs/levelParts/G6");
+        levelParts.Add(part6);
+        
         levelPartsPositions = new List<Vector3>();
 
         int partPiece = Random.Range(0, levelParts.Count-1);
@@ -49,37 +39,27 @@ public class GameGenerator : MonoBehaviour{
     }
 
     private void Update(){
-        print("Player.position: " + Player.position.x + " lastPos: " + lastPos.x);
-        if(lastPos.x - Player.position.x <= 500)
-        {
-            print("*levelparts[0]: " + levelParts[0]);
+        if(lastPos.x - Player.position.x <= 500){
             int partPiece = Random.Range(0, levelParts.Count - 1);
-            
-
             int partGapy = Random.Range(-20, 20);
             Vector3 meep = new Vector3(partGapx, partGapy);
-            //add transform to list or array
-            
+                       
             SpawnLevelPart(levelParts[partPiece], lastPos + meep);
             removeOutOfSight(lastPos + meep);
         }
     }
-    private void removeOutOfSight(Vector3 meep)
-    {
+
+    private void removeOutOfSight(Vector3 meep){
         print("removeoutdisght function vEctor3" + meep);
         levelPartsPositions.Add(meep);
 
         GameObject[] blub = GameObject.FindGameObjectsWithTag("RemoveOnWalk");
         GameObject wall = GameObject.Find("Wall");
-        //print("blub: " + blub[0] + blub[2] + blub[3] + blub[4] + blub[5]);
-        for (var i = 0; i < blub.Length; i++)
-        {
-            if(Player.position.x - blub[i].transform.position.x >= 500)
-            {
+        for (var i = 0; i < blub.Length; i++){
+            if(Player.position.x - blub[i].transform.position.x >= 500){
                 wall.transform.position = blub[i].transform.position;
                 wall.transform.position += new Vector3(60, 60);
-                Destroy(blub[i]);
-               
+                Destroy(blub[i]); 
             }
         }
     }
